ca260868a00a6838df059980932edca25269f393
Radoslav <cobinet59@gmail.com>
2024-07-28 14:59:58 +0000

    fixed arguments

--- immich_auto_stack.sh
+++ immich_auto_stack.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 
-args="--api_key $API_KEY --api_url $API_URL --skip_previous $SKIP_PREVIOUS"
+args="--api_key $API_KEY --api_url $API_URL"
+
+if [ ! -z "$SKIP_PREVIOUS" ]; then
+    args="--skip_previous $SKIP_PREVIOUS $args"
+fi
 
 BASEDIR=$(dirname "$0")
 echo $args | xargs python3 -u $BASEDIR/immich_auto_stack.py
